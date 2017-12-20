f0e6e328c4cb2c208a036e203568062799e1c23f
JesusLinares01 <34040095+JesusLinares01@users.noreply.github.com>
2017-12-20 14:59:12 -0600

    Numbers_diferent

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        int d;
+        double e;
+
+        d = N / 2;
+        e = (double) N / 2;
+
+        if (d - e == 0)
+        {
+            Console.WriteLine("Not Weird");
+        }
+        else
+        {
+                Console.WriteLine("Weird");
+        }
+
+        Console.ReadKey();
+    }
+}
